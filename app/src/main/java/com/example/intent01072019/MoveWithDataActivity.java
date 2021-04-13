7499fac30208e3d6f3a8f9e13ca62637e3a10c82
Rois <rois.khoiron@gmail.com>
2021-04-13 08:27:53 +0700

    final commit

---
+++ app/src/main/java/com/example/intent01072019/MoveWithDataActivity.java
@@ -0,0 +1,25 @@
+package com.example.intent01072019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MoveWithDataActivity extends AppCompatActivity {
+    public static final String EXTRA_AGE = "extra_age";
+    public static final String EXTRA_NAME = "extra_name";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_data);
+
+        TextView dataReceived = findViewById(R.id.data_recieved);
+
+        String name = getIntent().getStringExtra(EXTRA_NAME);
+        int age = getIntent().getIntExtra(EXTRA_AGE, 0);
+
+        String text = "Name : " + name + ",\nYour Age : " + age;
+        dataReceived.setText(text);
+    }
+}
